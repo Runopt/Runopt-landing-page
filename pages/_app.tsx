c4d03c01d374fc82ade7d686fb8d98428cfd9ad2
Kayphrosh <olakunbiolabode@yahoo.com>
2024-10-07 20:44:48 +0100

    runopt landing page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
-import "@/styles/globals.scss";
+import '@/styles/globals.scss';
 import '@/styles/main.scss';
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Runopt</title>
+        <meta name="description" content="Runopt website" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
