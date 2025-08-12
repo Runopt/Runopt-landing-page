b48a5e38c8399a4220b88b784860a98ecb413929
Stephan Lefort <lefortsge@gmail.com>
2025-08-12 15:56:36 -0700

    Implement SEO Meta tags for google search results

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,8 +7,29 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Runopt</title>
-        <meta name="description" content="Runopt website" />
+        <title>Runopt - Real Estate Feasibility Analysis</title>
+        <meta name="description" content="Advanced real estate feasibility analysis and civil engineering design platform" />
+
+        {/* SEO Meta Tags */}
+        <meta property="og:title" content="Runopt - Real Estate Feasibility Analysis" />
+        <meta property="og:description" content="Advanced real estate feasibility analysis and civil engineering design platform" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://runopt.co" />
+        <meta property="og:image" content="https://runopt.co/images/logo-seo.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:site_name" content="Runopt" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Runopt - Real Estate Feasibility Analysis" />
+        <meta name="twitter:description" content="Advanced real estate feasibility analysis and civil engineering design platform" />
+        <meta name="twitter:image" content="https://runopt.co/images/logo-seo.png" />
+
+        {/* Additional SEO Meta Tags */}
+        <meta name="keywords" content="real estate, feasibility analysis, civil engineering, design platform, construction, development" />
+        <meta name="author" content="Runopt" />
+        <meta name="robots" content="index, follow" />
+    
       </Head>
       <Component {...pageProps} />
     </>
